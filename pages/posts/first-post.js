1a626fbdcb5999c85d9cf16afc0d2067c8b650ee
Yu0614 <azu369yu@gmail.com>
2023-10-29 23:20:58 +0900

    feat: Navigate Between Pages section 4

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,3 +1,12 @@
+import Link from 'next/link';
+
 export default function FirstPost() {
-  return <h1>First Postだよ！</h1>;
+  return (
+    <div>
+      <h1>First Post</h1>
+      <h2>
+        <Link href="/">Back to home</Link>
+      </h2>
+    </div>
+  );
 }
